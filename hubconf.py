8b11abadfc3e7130c8f2926444e64f3b2c7fca58
Ari Iwunze <101911398+aritheanalyst@users.noreply.github.com>
2024-09-07 06:33:15 -0500

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -16,7 +16,7 @@ def unet(pretrained=False, **kwargs):
     model = UNet(**kwargs)
 
     if pretrained:
-        checkpoint = "weights/unet.pt"
+        checkpoint = "https://github.com/arithescientisttt/unet_brainsegmentation/blob/main/weights/unet.pt"
         state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=False, map_location='cpu')
         model.load_state_dict(state_dict)
 
